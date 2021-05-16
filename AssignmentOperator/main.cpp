cc87570f44ba2b9681bd037a3bb2e591c8621335
Robert Lowe <robertmlowe@rmlowe.com>
2021-05-16 19:52:54 +0800

    The Assignment Operator

---
+++ AssignmentOperator/main.cpp
@@ -0,0 +1,21 @@
+// Section 8
+// The Assignment Operator
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1 {10};
+    int num2 {20};
+    
+    100 = num1;
+    
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    
+    cout << endl;
+    return 0;
+}
+
